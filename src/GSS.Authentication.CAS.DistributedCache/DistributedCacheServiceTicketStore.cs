07afe01c957ce78811591c7e919cf8809002512f
Charley Wu <akunzai@gmail.com>
2018-10-18 15:46:54 +0800

    Add Custom CacheKeyFactory
    - Remove default constructor
    - Fix renewed cache never expired

--- src/GSS.Authentication.CAS.DistributedCache/DistributedCacheServiceTicketStore.cs
+++ src/GSS.Authentication.CAS.DistributedCache/DistributedCacheServiceTicketStore.cs
@@ -1,29 +1,24 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using System.Text;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Options;
 
 namespace GSS.Authentication.CAS
 {
     public class DistributedCacheServiceTicketStore : IServiceTicketStore
     {
-        protected const string Prefix = "cas-st";
-        protected IDistributedCache cache;
+        private const string Prefix = "cas-st";
+        private readonly IDistributedCache _cache;
 
-        public DistributedCacheServiceTicketStore()
+        public DistributedCacheServiceTicketStore(IDistributedCache cache)
         {
-            cache = new MemoryDistributedCache(Options.Create(new MemoryDistributedCacheOptions()));
+            _cache = cache;
         }
 
-        public DistributedCacheServiceTicketStore(IDistributedCache cacheClient)
-        {
-            this.cache = cacheClient;
-        }
+        public static Func<string, string> CacheKeyFactory { get; set; } = (key) => $"{Prefix}:{key}";
 
-        JsonSerializerSettings SerializerSettings { get; set; } = new JsonSerializerSettings
+        public static JsonSerializerSettings SerializerSettings { get; set; } = new JsonSerializerSettings
         {
             Formatting = Formatting.None,
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
@@ -34,9 +29,9 @@ namespace GSS.Authentication.CAS
         public async Task<string> StoreAsync(ServiceTicket ticket)
         {
             var value = Serialize(ticket);
-            await cache.SetAsync(
-                CombineKey(ticket.TicketId), 
-                value, 
+            await _cache.SetAsync(
+                CacheKeyFactory(ticket.TicketId),
+                value,
                 new DistributedCacheEntryOptions
                 {
                     AbsoluteExpiration = ticket.Assertion.ValidUntil
@@ -47,7 +42,7 @@ namespace GSS.Authentication.CAS
         public async Task<ServiceTicket> RetrieveAsync(string key)
         {
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
-            var value = await cache.GetAsync(CombineKey(key)).ConfigureAwait(false);
+            var value = await _cache.GetAsync(CacheKeyFactory(key)).ConfigureAwait(false);
             return Deserialize<ServiceTicket>(value);
         }
 
@@ -55,30 +50,28 @@ namespace GSS.Authentication.CAS
         {
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
             var value = Serialize(ticket);
-            return cache.RemoveAsync(CombineKey(key))
+            return _cache.RemoveAsync(CacheKeyFactory(key))
                 .ContinueWith(x =>
-               cache.SetAsync(CombineKey(key), value, new DistributedCacheEntryOptions()));
+               _cache.SetAsync(CacheKeyFactory(key), value, new DistributedCacheEntryOptions
+               {
+                   AbsoluteExpiration = ticket.Assertion.ValidUntil
+               }));
         }
 
         public Task RemoveAsync(string key)
         {
             if (string.IsNullOrEmpty(key)) throw new ArgumentNullException(nameof(key));
-            return cache.RemoveAsync(CombineKey(key));
+            return _cache.RemoveAsync(CacheKeyFactory(key));
         }
 
-        protected virtual string CombineKey(string key)
-        {
-            return $"{Prefix}:{key}";
-        }
-
-        protected virtual byte[] Serialize(object value)
+        private static byte[] Serialize(object value)
         {
             return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(value, SerializerSettings));
         }
 
-        protected virtual T Deserialize<T>(byte[] value)
+        private static T Deserialize<T>(byte[] value)
         {
-            if (value == null) return default(T);
+            if (value == null) return default;
             var json = Encoding.UTF8.GetString(value, 0, value.Length);
             return JsonConvert.DeserializeObject<T>(json, SerializerSettings);
         }

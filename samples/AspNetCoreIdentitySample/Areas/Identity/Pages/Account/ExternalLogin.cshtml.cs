b5035b39b4f6d7badfbf6264438bcdf80c65fafd
Charley Wu <akunzai@gmail.com>
2020-12-22 10:05:36 +0800

    Update Scaffold Identity in ASP.NET Core
    - https://docs.microsoft.com/aspnet/core/security/authentication/scaffold-identity

--- samples/AspNetCoreIdentitySample/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ samples/AspNetCoreIdentitySample/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -36,7 +36,7 @@ namespace AspNetCoreIdentitySample.Areas.Identity.Pages.Account
         [BindProperty]
         public InputModel Input { get; set; }
 
-        public string LoginProvider { get; set; }
+        public string ProviderDisplayName { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -93,7 +93,7 @@ namespace AspNetCoreIdentitySample.Areas.Identity.Pages.Account
             {
                 // If the user does not have an account, then ask the user to create an account.
                 ReturnUrl = returnUrl;
-                LoginProvider = info.LoginProvider;
+                ProviderDisplayName = info.ProviderDisplayName;
                 if (info.Principal.HasClaim(c => c.Type == ClaimTypes.Email))
                 {
                     Input = new InputModel
@@ -119,13 +119,13 @@ namespace AspNetCoreIdentitySample.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+
                 var result = await _userManager.CreateAsync(user).ConfigureAwait(false);
                 if (result.Succeeded)
                 {
                     result = await _userManager.AddLoginAsync(user, info).ConfigureAwait(false);
                     if (result.Succeeded)
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false).ConfigureAwait(false);
                         _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
 
                         var userId = await _userManager.GetUserIdAsync(user).ConfigureAwait(false);
@@ -134,11 +134,19 @@ namespace AspNetCoreIdentitySample.Areas.Identity.Pages.Account
                         var callbackUrl = Url.Page(
                             "/Account/ConfirmEmail",
                             pageHandler: null,
-                            values: new { area = "Identity", userId, code },
+                            values: new { area = "Identity", userId = userId, code = code },
                             protocol: Request.Scheme);
 
                         await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                             $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.").ConfigureAwait(false);
+
+                        // If account confirmation is required, we need to show the link if we don't have a real email sender
+                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                        {
+                            return RedirectToPage("./RegisterConfirmation", new { Email = Input.Email });
+                        }
+
+                        await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider).ConfigureAwait(false);
 
                         return LocalRedirect(returnUrl);
                     }
@@ -149,7 +157,7 @@ namespace AspNetCoreIdentitySample.Areas.Identity.Pages.Account
                 }
             }
 
-            LoginProvider = info.LoginProvider;
+            ProviderDisplayName = info.ProviderDisplayName;
             ReturnUrl = returnUrl;
             return Page();
         }

9dc85cf08247aa11291562104ac689a5e4b06c24
Charley Wu <akunzai@gmail.com>
2022-04-06 23:57:23 +0800

    User attributes may released in CAS v2 protocol with forward-compatible mode
    - https://apereo.github.io/cas/development/ux/User-Interface-Customization-Views.html#cas-v2

--- src/GSS.Authentication.CAS.Core/Validation/Cas20ServiceTicketValidator.cs
+++ src/GSS.Authentication.CAS.Core/Validation/Cas20ServiceTicketValidator.cs
@@ -1,18 +1,20 @@
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Security.Authentication;
 using System.Xml.Linq;
 using GSS.Authentication.CAS.Security;
+using Microsoft.Extensions.Primitives;
 
 namespace GSS.Authentication.CAS.Validation
 {
     // see https://apereo.github.io/cas/development/protocol/CAS-Protocol-Specification.html#25-servicevalidate-cas-20
     public class Cas20ServiceTicketValidator : CasServiceTicketValidator
     {
-        protected static XNamespace Namespace = "http://www.yale.edu/tp/cas";
-        protected static XName Attributes = Namespace + "attributes";
-        protected static XName AuthenticationSuccess = Namespace + "authenticationSuccess";
-        protected static XName AuthenticationFailure = Namespace + "authenticationFailure";
-        protected static XName User = Namespace + "user";
+        private static readonly XNamespace _namespace = "http://www.yale.edu/tp/cas";
+        private static readonly XName _attributes = _namespace + "attributes";
+        private static readonly XName _authenticationSuccess = _namespace + "authenticationSuccess";
+        private static readonly XName _authenticationFailure = _namespace + "authenticationFailure";
+        private static readonly XName _user = _namespace + "user";
 
         public Cas20ServiceTicketValidator(
             ICasOptions options,
@@ -39,12 +41,12 @@ namespace GSS.Authentication.CAS.Validation
               </cas:authenticationFailure>
             </cas:serviceResponse>
             */
-            var failureElement = doc.Element(AuthenticationFailure);
+            var failureElement = doc.Element(_authenticationFailure);
             if (failureElement != null)
             {
                 throw new AuthenticationException(failureElement.Value);
             }
-            /* On ticket validation success:
+            /* On ticket validation success
             <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
                 <cas:authenticationSuccess>
                 <cas:user>username</cas:user>
@@ -52,9 +54,39 @@ namespace GSS.Authentication.CAS.Validation
                 </cas:authenticationSuccess>
             </cas:serviceResponse>
             */
-            var principalName = doc.Element(AuthenticationSuccess).Element(User)?.Value ?? string.Empty;
+            var successElement = doc.Element(_authenticationSuccess);
+            if (successElement == null) return null;
+            var principalName = successElement.Element(_user)?.Value ?? string.Empty;
             if (string.IsNullOrWhiteSpace(principalName)) return null;
-            var assertion = new Assertion(principalName);
+            var attributes = new Dictionary<string, StringValues>();
+            var attributeElements = successElement.Element(_attributes)?.Elements();
+            /* User attributes may released in CAS v2 protocol with forward-compatible mode
+            <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
+                <cas:authenticationSuccess>
+                  <cas:user>username</cas:user>
+                  <cas:attributes>
+                    <cas:firstname>John</cas:firstname>
+                    <cas:lastname>Doe</cas:lastname>
+                    <cas:title>Mr.</cas:title>
+                    <cas:email>jdoe @example.org</cas:email>
+                    <cas:affiliation>staff</cas:affiliation>
+                    <cas:affiliation>faculty</cas:affiliation>
+                  </cas:attributes>
+                  <cas:proxyGrantingTicket>PGTIOU-84678-8a9d...</cas:proxyGrantingTicket>
+                </cas:authenticationSuccess>
+            </cas:serviceResponse>
+             */
+            if (attributeElements != null)
+            {
+                foreach (var attr in attributeElements)
+                {
+                    var name = attr.Name.LocalName;
+                    attributes[name] = attributes.ContainsKey(name)
+                        ? StringValues.Concat(attributes[name], attr.Value)
+                        : new StringValues(attr.Value);
+                }
+            }
+            var assertion = new Assertion(principalName, attributes);
             return new CasPrincipal(assertion, Options.AuthenticationType);
         }
     }
--- src/GSS.Authentication.CAS.Core/Validation/Cas30ServiceTicketValidator.cs
+++ src/GSS.Authentication.CAS.Core/Validation/Cas30ServiceTicketValidator.cs
@@ -1,9 +1,4 @@
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Security.Authentication;
-using System.Xml.Linq;
-using GSS.Authentication.CAS.Security;
-using Microsoft.Extensions.Primitives;
 
 namespace GSS.Authentication.CAS.Validation
 {
@@ -15,50 +10,6 @@ namespace GSS.Authentication.CAS.Validation
             HttpClient? httpClient = null)
             : base("p3/serviceValidate", options, httpClient)
         {
-        }
-
-        protected override ICasPrincipal? BuildPrincipal(string responseBody)
-        {
-            var doc = XElement.Parse(responseBody);
-            var failureElement = doc.Element(AuthenticationFailure);
-            if (failureElement != null)
-            {
-                throw new AuthenticationException(failureElement.Value);
-            }
-            /* On ticket validation success:
-            <cas:serviceResponse xmlns:cas="http://www.yale.edu/tp/cas">
-                <cas:authenticationSuccess>
-                  <cas:user>username</cas:user>
-                  <cas:attributes>
-                    <cas:firstname>John</cas:firstname>
-                    <cas:lastname>Doe</cas:lastname>
-                    <cas:title>Mr.</cas:title>
-                    <cas:email>jdoe @example.org</cas:email>
-                    <cas:affiliation>staff</cas:affiliation>
-                    <cas:affiliation>faculty</cas:affiliation>
-                  </cas:attributes>
-                  <cas:proxyGrantingTicket>PGTIOU-84678-8a9d...</cas:proxyGrantingTicket>
-                </cas:authenticationSuccess>
-            </cas:serviceResponse>
-            */
-            var successElement = doc.Element(AuthenticationSuccess);
-            if (successElement == null) return null;
-            var principalName = successElement.Element(User)?.Value ?? string.Empty;
-            if (string.IsNullOrWhiteSpace(principalName)) return null;
-            var attributes = new Dictionary<string, StringValues>();
-            var attributeElements = successElement.Element(Attributes)?.Elements();
-            if (attributeElements != null)
-            {
-                foreach (var attr in attributeElements)
-                {
-                    var name = attr.Name.LocalName;
-                    attributes[name] = attributes.ContainsKey(name)
-                        ? StringValues.Concat(attributes[name], attr.Value)
-                        : new StringValues(attr.Value);
-                }
-            }
-            var assertion = new Assertion(principalName, attributes);
-            return new CasPrincipal(assertion, Options.AuthenticationType);
         }
     }
 }

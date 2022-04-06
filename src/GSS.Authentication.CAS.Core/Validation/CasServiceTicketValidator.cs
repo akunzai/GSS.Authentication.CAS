ae828a0daa8ecd7389b7787c8fadc835fd14fac0
Charley Wu <akunzai@gmail.com>
2022-04-06 23:58:23 +0800

    Obsolete the ValidateUrlSuffix property

--- src/GSS.Authentication.CAS.Core/Validation/CasServiceTicketValidator.cs
+++ src/GSS.Authentication.CAS.Core/Validation/CasServiceTicketValidator.cs
@@ -13,13 +13,16 @@ namespace GSS.Authentication.CAS.Validation
 
         protected CasServiceTicketValidator(string suffix, ICasOptions options, HttpClient? httpClient = null)
         {
+#pragma warning disable CS0618
             ValidateUrlSuffix = suffix;
+#pragma warning restore CS0618
             Options = options ?? throw new ArgumentNullException(nameof(options));
             _httpClient = httpClient ?? new HttpClient();
         }
 
         protected ICasOptions Options { get; }
 
+        [Obsolete("Use constructor parameter instead")]
         protected string ValidateUrlSuffix { get; }
 
         public virtual async Task<ICasPrincipal?> ValidateAsync(string ticket, string service, CancellationToken cancellationToken)
@@ -27,7 +30,9 @@ namespace GSS.Authentication.CAS.Validation
             if (string.IsNullOrEmpty(ticket)) throw new ArgumentNullException(nameof(ticket));
             if (string.IsNullOrEmpty(service)) throw new ArgumentNullException(nameof(service));
             var baseUri = new Uri(Options.CasServerUrlBase + (Options.CasServerUrlBase.EndsWith("/", StringComparison.Ordinal) ? string.Empty : "/"));
+#pragma warning disable CS0618
             var validateUri = new Uri(baseUri, ValidateUrlSuffix);
+#pragma warning restore CS0618
             // unescape first to prevent double escape
             var escapedService = Uri.EscapeDataString(Uri.UnescapeDataString(service));
             var escapedTicket = Uri.EscapeDataString(ticket);

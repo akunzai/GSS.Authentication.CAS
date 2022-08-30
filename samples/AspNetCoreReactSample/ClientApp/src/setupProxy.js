4fca2c6d5410d350bd8b08d3958f222a14682755
Charley Wu <akunzai@gmail.com>
2022-08-30 11:07:54 +0800

    Fix typo

--- samples/AspNetCoreReactSample/ClientApp/src/setupProxy.js
+++ samples/AspNetCoreReactSample/ClientApp/src/setupProxy.js
@@ -11,7 +11,7 @@ const target = env.ASPNETCORE_HTTPS_PORT
   ? env.ASPNETCORE_URLS.split(';')[0]
   : 'http://localhost:5000';
 
-const context = ['/api/**', '/account/*', '/signin-*', 'signout-*'];
+const context = ['/api/**', '/account/*', '/signin-*', '/signout-*'];
 
 module.exports = function (app) {
   const appProxy = createProxyMiddleware(context, {

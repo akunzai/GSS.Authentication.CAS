f188c7983f4f169dc4701347cc345525144377d7
Charley Wu <akunzai@gmail.com>
2022-10-06 08:34:47 +0800

    Remove CodeQL analyze
    - CodeQL fails with incompatible project since C# autobuilder 2.11.0

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-dotnet build -c Release
-msbuild samples/OwinSample/OwinSample.csproj -noLogo -verbosity:minimal -restore
-msbuild samples/OwinSingleLogoutSample/OwinSingleLogoutSample.csproj -noLogo -verbosity:minimal -restore

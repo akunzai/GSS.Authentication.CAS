e3f1c42f2abe3024fd71e2f3a9b79d009c82e47e
Charley Wu <akunzai@gmail.com>
2022-10-23 12:46:43 +0800

    Remove the `react-service-container`

--- samples/AspNetCoreReactSample/ClientApp/src/App.tsx
+++ samples/AspNetCoreReactSample/ClientApp/src/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { ServiceContainer } from 'react-service-container';
-import { UserManager } from './api';
 import { Home, Layout, Login } from './components';
 
 export default function App(): JSX.Element {
   const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
   return (
     <BrowserRouter basename={baseUrl || ''}>
-      <ServiceContainer providers={[UserManager]}>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -15,7 +12,6 @@ export default function App(): JSX.Element {
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </Layout>
-      </ServiceContainer>
     </BrowserRouter>
   );
 }

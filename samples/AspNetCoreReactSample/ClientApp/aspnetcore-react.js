e92db0ec2e6302a4f835f12de415a2550769d326
Charley Wu <akunzai@gmail.com>
2023-07-06 08:14:11 +0800

    Migrate to Vite
    - https://vitejs.dev/

--- samples/AspNetCoreReactSample/ClientApp/aspnetcore-react.js
+++ samples/AspNetCoreReactSample/ClientApp/aspnetcore-react.js
@@ -1,8 +1,8 @@
 // This script configures the .env.development.local file with additional environment variables to configure HTTPS using the ASP.NET Core
 // development certificate in the webpack development proxy.
 
-const fs = require('fs');
-const path = require('path');
+import { existsSync, writeFileSync, readFileSync, appendFileSync } from 'fs';
+import { join } from 'path';
 
 const baseFolder =
   process.env.APPDATA !== undefined && process.env.APPDATA !== ''
@@ -23,17 +23,17 @@ if (!certificateName) {
   process.exit(-1);
 }
 
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+const certFilePath = join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync('.env.development.local')) {
-  fs.writeFileSync(
+if (!existsSync('.env.development.local')) {
+  writeFileSync(
     '.env.development.local',
     `SSL_CRT_FILE=${certFilePath}
 SSL_KEY_FILE=${keyFilePath}`
   );
 } else {
-  let lines = fs.readFileSync('.env.development.local').toString().split('\n');
+  let lines = readFileSync('.env.development.local').toString().split('\n');
 
   let hasCert,
     hasCertKey = false;
@@ -46,13 +46,13 @@ SSL_KEY_FILE=${keyFilePath}`
     }
   }
   if (!hasCert) {
-    fs.appendFileSync(
+    appendFileSync(
       '.env.development.local',
       `\nSSL_CRT_FILE=${certFilePath}`
     );
   }
   if (!hasCertKey) {
-    fs.appendFileSync(
+    appendFileSync(
       '.env.development.local',
       `\nSSL_KEY_FILE=${keyFilePath}`
     );

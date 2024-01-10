e3f3df31938a38da325fd9c857d6c883052fc524
Charley Wu <akunzai@gmail.com>
2024-01-10 23:43:13 +0800

    Remove the Form login from samples

--- samples/BlazorServerSample/Pages/Account/Login.cshtml.cs
+++ samples/BlazorServerSample/Pages/Account/Login.cshtml.cs
@@ -1,7 +1,4 @@
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,16 +8,6 @@ namespace BlazorServerSample.Pages.Account;
 [AllowAnonymous]
 public class Login : PageModel
 {
-    public string FormClass { get; set; } = "invisible";
-
-    [Required]
-    [BindProperty]
-    public string Username { get; set; } = default!;
-
-    [Required]
-    [BindProperty]
-    public string Password { get; set; } = default!;
-
     public IActionResult OnGet(string scheme)
     {
         if (string.IsNullOrWhiteSpace(scheme))
@@ -29,27 +16,5 @@ public class Login : PageModel
         }
 
         return Challenge(new AuthenticationProperties { RedirectUri = "/" }, scheme);
-    }
-
-    public IActionResult OnPost()
-    {
-        FormClass = string.Empty;
-
-        if (!ModelState.IsValid)
-        {
-            return Page();
-        }
-
-        if (!(Username == "test" && Password == "test"))
-        {
-            ModelState.AddModelError(string.Empty, "The username or password is incorrect");
-            return Page();
-        }
-
-        var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-        identity.AddClaim(new Claim(identity.NameClaimType, Username));
-        return SignIn(new ClaimsPrincipal(identity),
-            new AuthenticationProperties { RedirectUri = "/" },
-            identity.AuthenticationType ?? CookieAuthenticationDefaults.AuthenticationScheme);
     }
 }
--- samples/OwinSample/Controllers/AccountController.cs
+++ samples/OwinSample/Controllers/AccountController.cs
@@ -1,9 +1,6 @@
-using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.Owin.Security;
-using Microsoft.Owin.Security.Cookies;
-using OwinSample.Models;
 
 namespace OwinSample.Controllers
 {
@@ -20,30 +17,7 @@ namespace OwinSample.Controllers
             }
 
             Request.GetOwinContext().Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" }, scheme);
-            return new EmptyResult();
-        }
-
-        // POST /Account/Login
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Login(LoginViewModel model)
-        {
-            ViewData["formClass"] = string.Empty;
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-
-            if (!(model.Username == "test" && model.Password == "test"))
-            {
-                ModelState.AddModelError(string.Empty, "The username or password is incorrect");
-                return View();
-            }
-
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationType);
-            identity.AddClaim(new Claim(identity.NameClaimType, model.Username));
-            Request.GetOwinContext().Authentication.SignIn(identity);
-            return Redirect("/");
+            return new HttpUnauthorizedResult();
         }
 
         // GET /Account/Logout

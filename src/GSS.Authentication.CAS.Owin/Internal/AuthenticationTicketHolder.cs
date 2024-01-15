a6222e7ae4362112fd3d1bf3d8806d1d8f80da45
Charley Wu <akunzai@gmail.com>
2024-01-15 23:16:40 +0800

    Restore the authenticationType for `AuthenticationTicket`

--- src/GSS.Authentication.CAS.Owin/Internal/AuthenticationTicketHolder.cs
+++ src/GSS.Authentication.CAS.Owin/Internal/AuthenticationTicketHolder.cs
@@ -10,25 +10,26 @@ namespace GSS.Authentication.CAS.Owin.Internal
         public AuthenticationTicketHolder(AuthenticationTicket ticket)
         {
             AuthenticationType = ticket.Identity.AuthenticationType;
-            Claims = ticket.Identity.Claims.Select(x=>new ClaimHolder(x));
+            Claims = ticket.Identity.Claims.Select(x => new ClaimHolder(x));
             NameClaimType = ticket.Identity?.NameClaimType;
             RoleClaimType = ticket.Identity?.RoleClaimType;
             Properties = ticket.Properties.Dictionary;
         }
-    
+
         public string AuthenticationType { get; set; }
-    
+
         public IEnumerable<ClaimHolder> Claims { get; set; }
 
         public string? NameClaimType { get; set; }
 
         public string? RoleClaimType { get; set; }
-    
+
         public IDictionary<string, string> Properties { get; set; }
-    
+
         public static explicit operator AuthenticationTicket(AuthenticationTicketHolder h)
         {
-            var identity = new ClaimsIdentity(h.Claims.Select(x=>(Claim)x), h.AuthenticationType, h.NameClaimType, h.RoleClaimType);
+            var identity = new ClaimsIdentity(h.Claims.Select(x => (Claim)x), h.AuthenticationType, h.NameClaimType,
+                h.RoleClaimType);
             var properties = new AuthenticationProperties(h.Properties);
             return new AuthenticationTicket(identity, properties);
         }

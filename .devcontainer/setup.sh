cdd460dd814f03fd23d4546819640760e112b50b
Charley Wu <akunzai@gmail.com>
2024-09-14 18:58:31 +0800

    Fix lint warnings

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # https://learn.microsoft.com/aspnet/core/security/docker-compose-https#macos-or-linux
-if [ -f "/https/aspnetapp.pem" ] && [ -f "/https/aspnetapp.key" ]; then
+if [[ -f "/https/aspnetapp.pem" ]] && [[ -f "/https/aspnetapp.key" ]]; then
     cat >> /home/vscode/.bashrc <<EOF
 export ASPNETCORE_Kestrel__Certificates__Default__Path="/https/aspnetapp.pem"
 export ASPNETCORE_Kestrel__Certificates__Default__KeyPath="/https/aspnetapp.key"

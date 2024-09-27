c346c804906028a81a5c891e388a475e057dcd7f
Charley Wu <akunzai@gmail.com>
2024-09-27 09:15:09 +0800

    Add AspNetCoreMvc sample

---
+++ samples/AspNetCoreMvcSample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreMvcSample.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreMvcSample.Controllers;
+
+[AllowAnonymous]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
